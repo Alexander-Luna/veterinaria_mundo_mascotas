dfdfdc4912039a2b789826defd5e9412dd5d7d28
WILSON PAREDES <wilsonparedes87@gmail.com>
2022-08-25 15:38:52 -0500

    update componet form

--- frontend/src/components/Servicios/Formulario.jsx
+++ frontend/src/components/Servicios/Formulario.jsx
@@ -3,39 +3,40 @@ import '../Servicios/Formulario.css';
 import '../../styles/LoginAndRegister.css';
 import emailjs from 'emailjs-com';
 export default class Formulario extends Component {
-  
+
   render() {
-    
+
     function enviarEmail(e) {
       e.preventDefault();
-      emailjs.sendForm('default_service', 'template_7tdxet2',e.target,'DZqEOZRy1PkVFLYw-').then(re => {
+      emailjs.sendForm('default_service', 'template_7tdxet2', e.target, 'DZqEOZRy1PkVFLYw-').then(re => {
         alert("Email enviado ");
       }, (e) => {
         alert(JSON.stringify(e));
       }
       );
     }
+
     return (
       <div className='contenedor-formulario'>
         <form className='formulario' onSubmit={enviarEmail}>
           <div >
             <input id="name"
               type="text" name="name"
-              placeholder="Nombre" />
+              placeholder="Nombre" required/>
           </div>
           <div>
             <input id="last_name" type="text" name="last_name" placeholder="Apellido" />
           </div>
           <div>
-            <input id="email" type="text" placeholder='E-Mail' name="email" />
+            <input id="email" type="email" placeholder='E-Mail' name="email" required/>
           </div>
           <div>
-            <input id="phone" type="text" name="phone" placeholder="telefono" />
+            <input id="phone" type="text" name="phone" placeholder="telefono" required/>
           </div>
           <div>
-            <textarea name="message" id="message" className='contenedor-mensaje' rows="4" cols="40" placeholder="Mensaje"></textarea>
+            <textarea name="message" id="message" className='contenedor-mensaje' rows="4" cols="40" placeholder="Mensaje" required></textarea>
           </div>
-          <button className='envio-informacion' type='submit'>Enviar</button>
+          <button className='envio-informacion' type='submit' value="Validar Correo" >Enviar</button>
         </form>
       </div>
 
